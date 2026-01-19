24393818bf05104d4eace1798c6a1280a9fe1b01
Tanner Petreman <tanner.petreman@edu.sait.ca>
2026-01-19 11:48:43 -0700

    Create StudentInfo.js

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SimpleTextComponent() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <p>This is some basic text rendered in React.</p>
+    </div>
+  );
+}
+
+export default SimpleTextComponent;
